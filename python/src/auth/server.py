2515f2f26ba83f724d1a271517080f433ffa1548
ardas0002 <adrian17159@gmail.com>
2024-09-23 14:10:59 +0200

    auth service done

--- python/src/auth/server.py
+++ python/src/auth/server.py
@@ -34,4 +34,41 @@ def login():
     else:
         return "invalid credentials", 401
         
-        
+    
+def createJWT(username, secret, authz):
+    return jwt.encode(
+        {
+            "username": username,
+            "exp": datetime.datetime.now(tz=datetime.timezone.utc) + 
+            datetime.timedelta(days=1),
+            "iat": datetime.datetime.utcnow(),
+            "admin": authz,
+        },
+        secret,
+        algorithm="HS256"
+    )
+
+@server.route("/validate", methods=["POST"])
+def validate():
+    encoded_jwt = request.headers["Authorization"]
+
+    if not encoded_jwt:
+        return "missing credentials", 401
+
+    if not encoded_jwt.startswith("Bearer "):
+        return "invalid credentials", 401
+    
+    encoded_jwt = encoded_jwt.split(" ")[1]
+
+    try: 
+        decoded: jwt.decode(
+            encoded_jwt, os.environ.get("JWT_SECRET"), algorithm=["H256"]
+        )
+    except:
+        return "not authorized", 403
+    
+    return decoded, 200
+    
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=5000)
